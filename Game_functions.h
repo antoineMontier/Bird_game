b593c84763fa087cfd656a6499bcb037eabed402
Antoine Montier <115585369+antoineMontier@users.noreply.github.com>
2022-11-17 11:57:58 +0100

    Added reset in settings

--- Game_functions.h
+++ Game_functions.h
@@ -4,7 +4,7 @@
 #include "SDL_Basics.h"
 
 #define BIRD_MAX_SIZE 200
-#define MAX_JUMP 200
+#define MAX_JUMP 50
 #define FRAMES_PER_SECOND 36
 #define BUTTON_WIDTH 70
 #define BUTTON_HEIGHT 70
@@ -15,7 +15,11 @@
 #define BIRD_MAX_SPEED 30
 #define MAX_GRAVITY 50
 #define MIN_SPIKE_DIFFICULTY 20
-
+#define ORIGINAL_BIRD_SPEED 8
+#define ORIGINAL_GRAVITY 9.81
+#define ORIGINAL_JUMP_PWR 20
+#define ORIGINAL_BIRD_SIZE 40
+#define ORIGINAL_SPIKE_INCREASE 7
 
 
 typedef struct{
@@ -37,7 +41,6 @@ typedef struct{
     unsigned int b;//blue 0-255
 }Color;
 
-
 void drawLandscape(SDL_Renderer* r, Color*c, int p);
 void printRestartButton(SDL_Renderer* r, Color*c, int p);
 void jump(double*y, double*vy, int*sj);
@@ -50,16 +53,4 @@ int birdTouchSpike(bird b, int facing, int spike_sz, int *s_l, int*s_r, int spik
 void startGame(bird*b, int*facing, int*pfacing, int*palette, int*lvl, double*a_l, double*a_r, int*u_l, int*u_r, int*s_l, int*s_r, int sn, int*jumped, int*menu, double bird_speed, double bird_size);
 void printSettingButton(SDL_Renderer* r, Color*c, int p);
 void printReturnButton(SDL_Renderer* r, Color*c, int p);
-
-
-
-
-
-
-
-
-
-
-
-
-
+void printResetSettingsButton(SDL_Renderer* r, TTF_Font*f, Color*c, int p);
