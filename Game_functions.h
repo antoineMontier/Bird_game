bc72cb402ed980b5bcecee972c474a9aaccfa448
Antoine Montier <115585369+antoineMontier@users.noreply.github.com>
2022-11-18 12:20:28 +0100

    Animation avaible in settings

--- Game_functions.h
+++ Game_functions.h
@@ -15,11 +15,13 @@
 #define BIRD_MAX_SPEED 30
 #define MAX_GRAVITY 50
 #define MIN_SPIKE_DIFFICULTY 20
+#define MAX_ANIMATION_SPEED 200
 #define ORIGINAL_BIRD_SPEED 8
 #define ORIGINAL_GRAVITY 9.81
 #define ORIGINAL_JUMP_PWR 20
 #define ORIGINAL_BIRD_SIZE 40
 #define ORIGINAL_SPIKE_INCREASE 7
+#define ORIGINAL_ANIMATION_SPEED 50
 
 
 typedef struct{
@@ -51,8 +53,8 @@ void drawBird(SDL_Renderer* r, bird b, int facing, int*j, Color*c, int p, double
 void moveBird(bird *b, int *facing, int* lvl, int size, double sp_sz, double bird_speed, double gravity, double bird_size);
 int birdTouchSpike(bird b, int facing, int spike_sz, int *s_l, int*s_r, int spike_nb, double bird_size);
 void startGame(bird*b, int*facing, int*pfacing, int*palette, int*lvl, double*a_l, double*a_r, int*u_l, int*u_r, int*s_l, int*s_r, int sn, int*jumped, double*menu, double bird_speed, double bird_size);
-void printSettingButton(SDL_Renderer* r, Color*c, int p);
+void printSettingButton(SDL_Renderer* r, Color*c, int p, double animation);
 void printReturnButton(SDL_Renderer* r, Color*c, int p, double animation);
 void printResetSettingsButton(SDL_Renderer* r, TTF_Font*f, Color*c, int p, double animation);
-void resetSettingsAndCursors(int*cursor_positions, double*bsp, double*g, double*jp, double*bsz, int*si, int spike_size);
-void printSettingMenu(SDL_Renderer* r, TTF_Font*big, TTF_Font*small, TTF_Font*score, int*cursor_positions, int spike_size, Color*c, int p, char*tmp, double*birdsize, double*menu, int lvl, int*s_l, int*s_r, int*spike_nb, bird b, int facing, int*j, double bird_size);
+void resetSettingsAndCursors(int*cursor_positions, double*bsp, double*g, double*jp, double*bsz, int*si, int*a, int spike_size);
+void printSettingMenu(SDL_Renderer* r, TTF_Font*big, TTF_Font*small, TTF_Font*score, int*cursor_positions, int spike_size, Color*c, int p, char*tmp, double*birdsize, double*menu, int lvl, int*s_l, int*s_r, int*spike_nb, bird b, int facing, int*j, double bird_size, int *animation);
